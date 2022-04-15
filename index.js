629caed2ddfa8c5f06b50ecf3b382228c1e9ebd4
Cengiz Bugra Aksakal <aksakalcengiz@hotmail.com>
2022-04-15 17:58:26 +0300

    commit tree 9227600ec2bc583c3c2b9b5917670ba615e34dff

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(headers => headers.split(' ').map(word => word = word[0].toUpperCase() + word.slice(1)).join(' '));
 }
+
+// console.log(titleCased());
